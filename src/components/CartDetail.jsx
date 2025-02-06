1f69148b10be6f3831e99b6d3a61f5891a478be3
Kader3wa <kader.bendahmane@3wa.io>
2025-02-06 15:23:07 +0100

    ajout reducer, context, page produits, details panier, total

--- src/components/CartDetail.jsx
+++ src/components/CartDetail.jsx
@@ -1,8 +1,12 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
 import { CartContext } from "../contexts/CartContext"
 
 const CartDetail = () => {
     const cartContext = useContext(CartContext);
+
+    const totalCart = () => {
+        return cartContext.state.products.reduce((acc, product) => acc + product.prix, 0) + " €";
+    }
 
     const handleRemoveToCart = (product) => {
         cartContext.dispatch({
@@ -13,32 +17,37 @@ const CartDetail = () => {
 
     return (
         <>
-            <section id="cart-details">
-                <h1>Détails du panier</h1>
-                <h2 className="badge"></h2>
-                <div className="products">
-                    {
-                        cartContext.state.products.map((product) => {
-                            return (
-                                <div className="card" key={product.id}>
-                                    <img src={product.image} alt={product.nom} className="card-img" />
-                                    <div className="card-content">
-                                        <span>
-                                            {product.nom}
-                                        </span>
-                                        <span className="badge">
-                                            {product.prix} €
-                                        </span>
+            {cartContext.state.products.length > 0 &&
+                <section id="cart-details">
+                    <h2>Détails du panier
+                        <span className="badge total">
+                            {totalCart()}
+                        </span>
+                    </h2>
+                    <div className="products">
+                        {
+                            cartContext.state.products.map((product) => {
+                                return (
+                                    <div className="card" key={product.id}>
+                                        <img src={product.image} alt={product.nom} className="card-img" />
+                                        <div className="card-content">
+                                            <span>
+                                                {product.nom}
+                                            </span>
+                                            <span className="badge">
+                                                {product.prix} €
+                                            </span>
+                                        </div>
+                                        <button className="btn-add-cart" onClick={() => handleRemoveToCart(product)}>
+                                            Retirer
+                                        </button>
                                     </div>
-                                    <button className="btn-add-cart" onClick={() => handleRemoveToCart(product)}>
-                                        Retirer
-                                    </button>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </section>
+                                )
+                            })
+                        }
+                    </div>
+                </section>
+            }
         </>
     );
 }
